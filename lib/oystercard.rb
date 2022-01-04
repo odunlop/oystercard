aab7ecaac8350ce2cebd5d20aa82c25dbd0290fd
Orla <orladunlop2@gmail.com>
2022-01-04 14:43:51 +0000

    Allow user to top up their balance (fully tested)

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,4 +4,8 @@ class Oystercard
   def initialize
     @balance = 0
   end
+
+  def top_up(num)
+    @balance += num
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,19 @@
 require "oystercard"
 
 describe Oystercard do
-  it "initially has a balance of 0" do
-    expect(subject.balance).to eq 0
+
+  describe "#balance" do
+    it "initially has a balance of 0" do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe "#top_up" do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it "allows the user to top up their balance" do
+      subject.top_up(5)
+      expect(subject.balance).to eq 5
+    end
   end
 end
